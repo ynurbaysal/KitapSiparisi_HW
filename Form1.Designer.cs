d1d420ccdc1d7c327fd367fac4b856886a8d1fba
nurbaysal <nurbysl__@hotmail.com>
2019-07-17 18:41:45 +0300

    Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace KitapSiparisi_HW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFiyatHesapla = new System.Windows.Forms.Button();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFiyatHesapla
+            // 
+            this.btnFiyatHesapla.Location = new System.Drawing.Point(24, 121);
+            this.btnFiyatHesapla.Name = "btnFiyatHesapla";
+            this.btnFiyatHesapla.Size = new System.Drawing.Size(174, 23);
+            this.btnFiyatHesapla.TabIndex = 0;
+            this.btnFiyatHesapla.Text = "Fiyat Hesapla";
+            this.btnFiyatHesapla.UseVisualStyleBackColor = true;
+            this.btnFiyatHesapla.Click += new System.EventHandler(this.BtnFiyatHesapla_Click);
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(98, 61);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 20);
+            this.txtFiyat.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kitap Adedi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.btnFiyatHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFiyatHesapla;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitapSiparisi_HW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Disaridan siparis alinacak olan kitap miktari girilsin.
+            //Sipari sayisi 20'den azsa toplam ucretten %5, 20 - 50 araliginda ise %10, 
+            //50 -100 araligi ise %15, 100'den fazla ise %25 indirim yapilsin.
+            //Kitabın birim fiyatı => 5 TLdir...
+            //Amac => Odenecek tutari kullaniciya gostermek...
+        }
+
+        private void BtnFiyatHesapla_Click(object sender, EventArgs e)
+        {
+            int KitapAdedi = int.Parse(txtFiyat.Text);
+            txtFiyat.Focus();
+            txtFiyat.Clear();
+
+
+            if (KitapAdedi<=20)
+            {
+                double hesap = (KitapAdedi * 5) * 0.95;
+                MessageBox.Show("Ucretiniz "+ hesap);
+            }
+            if (KitapAdedi>20 && KitapAdedi<=50)
+            {
+                double hesap = (KitapAdedi * 5) * 0.90;
+                MessageBox.Show("Ucretiniz "+ hesap);
+            }
+            if (KitapAdedi>50 && KitapAdedi<=100)
+            {
+                double hesap = (KitapAdedi*5) * 0.85;
+                MessageBox.Show("Ucretiniz "+ hesap);
+            }
+            if (KitapAdedi>100)
+            {
+                double hesap = (KitapAdedi*5) * 0.75;
+                MessageBox.Show("Ucretiniz "+ hesap);
+            }
+
+
+
+
+
+
+        }
+    }
+}
+
+        
